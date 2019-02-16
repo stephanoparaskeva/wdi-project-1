b9c50d1635f4a29a90b002593512dfab89bb3fb4
Stephano Paraskeva <stephanokparaskeva@gmail.com>
2019-02-16 22:11:21 +0000

    finalised tasks, cleaned up code

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+  // htmlY[positionY].children[positionX].innerHTML = 'hello'
 
-  const htmlY = document.querySelectorAll('.Y')
-
-
-// myDiv.querySelectorAll("#myDiv > .foo");
-
-// htmlY[positionY].children[positionX].innerHTML = 'hello'
-
-
-  const lilAxis = [1,0,0,0,0,0,0,0,0,1]
   const axis = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -48,21 +40,11 @@ window.addEventListener('DOMContentLoaded', () => {
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
   ]
+
+  const htmlY = document.querySelectorAll('.Y')
+
   let positionX = 1
   let positionY = 1
-  console.log(axis)
-  // class Movement {
-  //   constructor() {
-  //     // this.xDirection = xDirection
-  //     // this.yDirection = yDirection
-  //     // this.startFrom = startFrom
-  //     // this.rightLeft = rightLeft
-  //
-  //     this.firstUpLeft = position - 1
-  //     this.firstDownRight = position + 1
-  //
-  //   }
-
   let moveX
   let pauseX
   let moveY
@@ -79,25 +61,18 @@ window.addEventListener('DOMContentLoaded', () => {
       htmlY[positionY-1].children[positionX].style.background = 'red'
       htmlY[positionY-1].children[positionX-1].style.background = 'red'
       axis[positionY][positionX] = 2
-      console.log(positionX)
-      console.log(positionY)
-
     }, 200)
     pauseX = setInterval(function() {
       if (axis[positionY][downRight ? positionX +1 : positionX -1] !== 0) {
         clearInterval(pauseX)
         clearInterval(moveX)
       }
-      // htmlY[positionY].children[positionX].style.background = 'white'
+      htmlY[positionY].children[positionX].style.background = 'white'
       htmlY[positionY].children[positionX-1].style.background = 'white'
       htmlY[positionY-1].children[positionX].style.background = 'white'
       htmlY[positionY-1].children[positionX-1].style.background = 'white'
-      console.log(positionX, 'positionX')
-      console.log(positionY, 'positionY')
       axis[positionY][positionX] = 0
       downRight ? ++positionX : --positionX //this works
-      console.log(downRight) //downRight works
-      // console.log(axis)
     }, 260)
   }
 
@@ -113,164 +88,67 @@ window.addEventListener('DOMContentLoaded', () => {
       htmlY[positionY-1].children[positionX].style.background = 'red'
       htmlY[positionY-1].children[positionX-1].style.background = 'red'
       axis[positionY][positionX] = 2
-      console.log(positionX)
-      console.log(positionY)
-
     }, 200)
     pauseY = setInterval(function() {
       if (axis[downRight ? positionY +1 : positionY -1][positionX] !== 0) {
         clearInterval(pauseY)
         clearInterval(moveY)
       }
-      // htmlY[positionY].children[positionX].style.background = 'white'
+      htmlY[positionY].children[positionX].style.background = 'white'
       htmlY[positionY].children[positionX-1].style.background = 'white'
       htmlY[positionY-1].children[positionX].style.background = 'white'
       htmlY[positionY-1].children[positionX-1].style.background = 'white'
       axis[positionY][positionX] = 0
       downRight ? ++positionY : --positionY
-
-      // console.log(axis)
-      // console.log(position)
     }, 260)
   }
-
-
-
-
-// const items = document.querySelector('.item1')
-//   items.addEventListener('click', () => {
-//     console.log(position)
-//     wayToMove(true)
-//   })
-//
-// const itemsvlV = document.querySelector('.item2')
-//   itemsvlV.addEventListener('click', () => {
-//     console.log(position)
-//     wayToMove(false)
-//   })
-
-  // document.onkeydown('onkeydown', (e) => {
-  //   if (e.KeyCode === 37) {
-  //     wayToMove(axis, false)
-  //   }
-  //   if (e.KeyCode === 38) {
-  //     wayToMove(yAxis, false)
-  //   }
-  //   if (e.KeyCode === 39) {
-  //     wayToMove(xAxis, true)
-  //   }
-  //   if (e.KeyCode === 40) {
-  //     wayToMaxisAxis, true)
-  //   }
-  // })
 
   document.onkeydown = function(e) {
     e = e || window.event
     switch(e.which || e.keyCode) {
       case 37:
-      clearInterval(moveX)
-      clearInterval(pauseX)
-      clearInterval(moveY)
-      clearInterval(pauseY)
+        clearInterval(moveX)
+        clearInterval(pauseX)
+        clearInterval(moveY)
+        clearInterval(pauseY)
         leftRight(axis, false)
 
         break
 
       case 38:
-      clearInterval(moveX)
-      clearInterval(pauseX)
-      clearInterval(moveY)
-      clearInterval(pauseY)
+        clearInterval(moveX)
+        clearInterval(pauseX)
+        clearInterval(moveY)
+        clearInterval(pauseY)
         upDown(axis, false)
 
         break
 
       case 39:
-      clearInterval(moveX)
-      clearInterval(pauseX)
-      clearInterval(moveY)
-      clearInterval(pauseY)
+        clearInterval(moveX)
+        clearInterval(pauseX)
+        clearInterval(moveY)
+        clearInterval(pauseY)
         leftRight(axis, true)
 
         break
 
       case 40:
-      clearInterval(moveX)
-      clearInterval(pauseX)
-      clearInterval(moveY)
-      clearInterval(pauseY)
+        clearInterval(moveX)
+        clearInterval(pauseX)
+        clearInterval(moveY)
+        clearInterval(pauseY)
         upDown(axis, true)
 
         break
 
-      default: return // xit this handler for other keys
+      default: return
     }
-    e.preventDefault() // prevent the default action (scroll / move caret)
+    e.preventDefault()
   }
 
 
 
-// arrays from both sides
-// y axis
-// event listeners
-// stop addEventListener
 
 
-
-
-
-
-  // const firstTest = [0,0,0,0,0,0,0,0,0,1]
-  // let index = 0
-  //
-  // const move = setInterval(function() {
-  //   if (firstTest[index+1] !== 0) {
-  //     clearInterval(moveX)
-  //   }
-  //   firstTest[index] = 1
-  //   console.log(firstTest)
-  // }, 200)
-  // const pause = setInterval(function() {
-  //   if (firstTest[index+1] !== 0) {
-  //     clearInterval(pauseX)
-  //   }
-  //   firstTest[index] = 0
-  //   index ++
-  // }, 200)
-  //
-  //
-  //
-  // const firstTest = [0,0,0,0,0,0,0,0,0,0]
-  // let index = firstTest.length-1
-  //
-  // const moveX = setInterval(function() {
-  //   if (firstTest[index-1] !== 0) {
-  //     clearInterval(moveX)
-  //   }
-  //   firstTest[index] = 1
-  //   console.log(firstTest)
-  // }, 200)
-  // const pauseX = setInterval(function() {
-  //   if (firstTest[index-1] !== 0) {
-  //     clearInterval(pauseX)
-  //   }
-  //   firstTest[index] = 0
-  //   index --
-  // }, 200)
-  //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+}) // close for DOMContentLoaded
