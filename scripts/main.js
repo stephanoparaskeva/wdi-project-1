f14136d88e5d0621948afc205bc40744e90b881f
Stephano Paraskeva <stephanokparaskeva@gmail.com>
2019-02-16 15:30:33 +0000

    removed constructor, created general movement function

--- scripts/main.js
+++ scripts/main.js
@@ -1,68 +1,145 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  const firstTest = [0,0,0,0,0,0,0,0,0,1]
-  let position = 0
-  class Movement {
-    constructor() {
-      // this.xDirection = xDirection
-      // this.yDirection = yDirection
-      // this.startFrom = startFrom
-      // this.rightLeft = rightLeft
+  const xAxis = [1,0,0,0,0,0,0,0,0,1]
+  const yAxis = [
+    [1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1]
+]
+  let position = 5
+  // class Movement {
+  //   constructor() {
+  //     // this.xDirection = xDirection
+  //     // this.yDirection = yDirection
+  //     // this.startFrom = startFrom
+  //     // this.rightLeft = rightLeft
+  //
+  //     this.firstUpLeft = position - 1
+  //     this.firstDownRight = position + 1
+  //
+  //   }
 
-      this.firstDownLeft = position - 1
-      this.incrementLeft = position - 1
-      this.firstUpRight = position + 1
-      this.incrementRight = position + 1
-    }
+  let move
+  let pause
 
-      wayToMove = function(firstMove, incrementMoves, position) {
-        console.log(firstMove)
-        console.log(incrementMoves)
-        console.log(position)
-
-        const moveX = setInterval(function() {
-          if (firstTest[position+1] !== 0) {
-            clearInterval(moveX)
-          }
-          firstTest[position] = 1
-          console.log(firstTest[firstMove])
-          console.log(firstTest)
-          
-        }, 200)
-        const pauseX = setInterval(function() {
-          if (firstTest[position+1] !== 0) {
-            clearInterval(pauseX)
-          }
-          firstTest[position] = 0
-          ++position
-          console.log(position)
-        }, 200)
+  const wayToMove = function(axis, downRight) {
+    move = setInterval(function() {
+      if (axis[downRight ? position + 1 : position - 1] !== 0) {
+        clearInterval(move)
+        clearInterval(pause)
       }
+      axis[position] = 2
 
+      console.log(axis)
+    }, 200)
+    pause = setInterval(function() {
+      if (axis[downRight ? position +1 : position -1] !== 0) {
+        clearInterval(pause)
+        clearInterval(move)
+      }
+      axis[position] = 0
+      downRight ? ++position : --position
+      console.log(position)
+    }, 200)
   }
 
-  const xMovementRight = new Movement()
-  xMovementRight.wayToMove(xMovementRight.firstUpRight, xMovementRight.incrementRight, position)
+console.log(pause)
+
+
+// const items = document.querySelector('.item1')
+//   items.addEventListener('click', () => {
+//     console.log(position)
+//     wayToMove(true)
+//   })
+//
+// const itemsvlV = document.querySelector('.item2')
+//   itemsvlV.addEventListener('click', () => {
+//     console.log(position)
+//     wayToMove(false)
+//   })
+
+  // document.onkeydown('onkeydown', (e) => {
+  //   if (e.KeyCode === 37) {
+  //     wayToMove(xAxis, false)
+  //   }
+  //   if (e.KeyCode === 38) {
+  //     wayToMove(yAxis, false)
+  //   }
+  //   if (e.KeyCode === 39) {
+  //     wayToMove(xAxis, true)
+  //   }
+  //   if (e.KeyCode === 40) {
+  //     wayToMove(yAxis, true)
+  //   }
+  // })
+
+  document.onkeydown = function(e) {
+    e = e || window.event
+    switch(e.which || e.keyCode) {
+      case 37:
+      clearInterval(move)
+      clearInterval(pause)
+        wayToMove(xAxis, false)
+
+        break
+
+      case 38:
+        clearInterval(move)
+        clearInterval(pause)
+        wayToMove(yAxis, false)
+
+        break
+
+      case 39:
+      clearInterval(move)
+      clearInterval(pause)
+        wayToMove(xAxis, true)
+
+        break
+
+      case 40:
+      clearInterval(move)
+      clearInterval(pause)
+        wayToMove(yAxis, true)
+
+        break
+
+      default: return // xit this handler for other keys
+    }
+    e.preventDefault() // prevent the default action (scroll / move caret)
+  }
+
+
+
+// arrays from both sides
+// y axis
+// event listeners
+// stop addEventListener
 
 
 
 
 
 
-
-
-  const items = document.querySelectorAll('.item')
   // const firstTest = [0,0,0,0,0,0,0,0,0,1]
   // let index = 0
   //
-  // const moveX = setInterval(function() {
+  // const move = setInterval(function() {
   //   if (firstTest[index+1] !== 0) {
   //     clearInterval(moveX)
   //   }
   //   firstTest[index] = 1
   //   console.log(firstTest)
   // }, 200)
-  // const pauseX = setInterval(function() {
+  // const pause = setInterval(function() {
   //   if (firstTest[index+1] !== 0) {
   //     clearInterval(pauseX)
   //   }
@@ -94,7 +171,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-console.log(firstTest)
+
 
 
 
